9ce2db551bc34d4912c4268cd8b78f54c3f53efa
HienTK <hientkhe160222@fpt.com.vn>
2024-07-03 22:50:16 +0700

    upload blog feature

---
+++ src/java/controller/blog/Blog.java
@@ -0,0 +1,141 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller.blog;
+
+import dal.implement.BlogDAO;
+import model.BlogTag;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.Part;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author hienhack
+ */
+public class Blog extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException, Exception {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            
+            String contextPath = request.getContextPath();
+            response.sendRedirect(contextPath + "/writtenblog");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(Blog.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(Blog.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String uploadFolder = request.getServletContext().getRealPath("assets/images/blog");
+        Path uploadPath = Paths.get(uploadFolder);
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath); // nếu thư mục upload không tồn tại, tạo mới
+        }
+
+        Part imagePart = request.getPart("image"); // lấy phần tử 'image' từ yêu cầu
+        String imageFilename = request.getParameter("currentImageFilename"); // tên tệp tin ảnh
+        if (imagePart != null && imagePart.getSize() > 0) {
+            imageFilename = Paths.get(imagePart.getSubmittedFileName()).getFileName().toString(); // lấy tên tệp tin mới
+            imagePart.write(Paths.get(uploadPath.toString(), imageFilename).toString()); // lưu tệp tin vào thư mục upload
+        }
+        String action = request.getParameter("action");
+        if (action != null && !action.isEmpty()) {
+            String title = request.getParameter("title");
+            String date = request.getParameter("date");
+            String detail = request.getParameter("texttitle");
+            String status = "";
+            String tag = request.getParameter("tag");
+            if (action.equals("save")) {
+                status = "0";
+            } else if (action.equals("publish")) {
+                status = "1";
+            }
+            HttpSession session = request.getSession();
+//            User a = (User) session.getAttribute("teacher");
+//            int uid = a.getUserID();
+
+            BlogDAO dao = new BlogDAO();
+
+            String blogIdParam = request.getParameter("blogid");
+            if (blogIdParam != null && !blogIdParam.isEmpty()) {
+                int blogId = Integer.parseInt(blogIdParam);
+                dao.updateBlog(title, imageFilename, detail, status, tag, blogId);
+            } else {
+                dao.insertBlog(title, imageFilename, detail, status, tag, 3);
+            }
+        }
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(Blog.class.getName()).log(Level.SEVERE, null, ex);
+            // Xử lý lỗi nếu cần thiết
+        } catch (Exception ex) {
+            Logger.getLogger(Blog.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/java/dal/implement/BlogDAO.java
@@ -0,0 +1,340 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal.implement;
+
+import model.Blog;
+import model.BlogTag;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import dal.DBContext;
+
+/**
+ *
+ * @author hienhack
+ */
+public class BlogDAO {
+
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    public List<Blog> getAllBlog() {
+        ArrayList<Blog> blogS = new ArrayList<>();
+        String query = "select * from blog WHERE BlogStatus = 1 Order By BlogId Desc";
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(query);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                int blogId = rs.getInt("BlogID");
+                String blogTitle = rs.getString("BlogTitle");
+                String blogImage = rs.getString("BlogImage");
+                String blogDetail = rs.getString("BlogDetail");
+                Date postDate = rs.getDate("PostDate");
+                int blogTagId = rs.getInt("BlogTagID"); // Lấy BlogTagID từ cơ sở dữ liệu
+                boolean status = rs.getBoolean("BlogStatus");
+                int uid = rs.getInt("UserID");
+
+                // Tạo đối tượng Blog từ dữ liệu từ cơ sở dữ liệu
+                Blog blog = new Blog(blogId, blogTitle, blogImage, blogDetail, postDate, blogTagId, status, uid);
+                blogS.add(blog);
+            }
+        } catch (Exception e) {
+        }
+        return blogS;
+    }
+
+    public List<Blog> searchBlogs(String query) throws Exception {
+        List<Blog> blogs = new ArrayList<>();
+        String sql = "SELECT * FROM blog WHERE BlogTitle LIKE ? OR BlogDetail LIKE ?";
+
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(sql);
+            String searchQuery = "%" + query + "%";
+            ps.setString(1, searchQuery);
+            ps.setString(2, searchQuery);
+
+             rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Blog blog = new Blog();
+                blog.setBlogID(rs.getInt("blogID"));
+                blog.setBlogTitle(rs.getString("BlogTitle"));
+                blog.setBlogDetail(rs.getString("BlogDetail"));
+                blog.setBlogImage(rs.getString("BlogImage"));
+                blog.setPostDate(rs.getDate("PostDate"));
+                blogs.add(blog);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return blogs;
+    }
+
+    public List<Blog> getRadomBlog() {
+        ArrayList<Blog> blogS = new ArrayList<>();
+        String query = "SELECT TOP 3 * FROM blog WHERE BlogStatus = 1\n"
+                + "ORDER BY NEWID()";
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(query);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                int blogId = rs.getInt("BlogID");
+                String blogTitle = rs.getString("BlogTitle");
+                String blogImage = rs.getString("BlogImage");
+                String blogDetail = rs.getString("BlogDetail");
+                Date postDate = rs.getDate("PostDate");
+                int blogTagId = rs.getInt("BlogTagID"); // Lấy BlogTagID từ cơ sở dữ liệu
+                boolean status = rs.getBoolean("BlogStatus");
+                int uid = rs.getInt("UserID");
+
+                // Tạo đối tượng Blog từ dữ liệu từ cơ sở dữ liệu
+                Blog blog = new Blog(blogId, blogTitle, blogImage, blogDetail, postDate, blogTagId, status, uid);
+                blogS.add(blog);
+            }
+        } catch (Exception e) {
+        }
+        return blogS;
+    }
+
+    public void insertBlog(String title, String image, String detail, String status, String blogtagid, int uid) {
+        String query = "INSERT INTO blog (BlogTitle, BlogImage, BlogDetail, PostDate, BlogStatus, BlogTagID, UserID) "
+                + "VALUES (?, ?, ?, GETDATE(), ?, ?, ?)";
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(query);
+            ps.setString(1, title);
+            ps.setString(2, image);
+            ps.setString(3, detail);
+            ps.setString(4, status);
+            ps.setString(5, blogtagid);
+            ps.setInt(6, uid);
+
+            ps.executeUpdate();  // Use executeUpdate() for INSERT statement
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Handle the exception appropriately
+        }
+    }
+
+    public List<BlogTag> getListBlogTag() throws SQLException, Exception {
+        List<BlogTag> list = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM blogtag";
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+
+                list.add(new BlogTag(rs.getInt(1), rs.getString(2)));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (ps != null) {
+                ps.close();
+            }
+            if (rs != null) {
+                rs.close();
+            }
+        }
+        return list;
+    }
+
+    public ArrayList<Blog> getBlogFromStatus(int status, int uid) throws Exception {
+        ArrayList<Blog> blogS = new ArrayList<>();
+        String sql = "SELECT * FROM blog WHERE BlogStatus = ? AND UserId = ?";
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, status);
+            ps.setInt(2, uid);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                int blogId = rs.getInt("BlogID");
+                String blogTitle = rs.getString("BlogTitle");
+                String blogImage = rs.getString("BlogImage");
+                String blogDetail = rs.getString("BlogDetail");
+                Date postDate = rs.getDate("PostDate");
+                int blogTagId = rs.getInt("BlogTagID"); // Lấy BlogTagID từ cơ sở dữ liệu
+
+                // Tạo đối tượng Blog từ dữ liệu từ cơ sở dữ liệu
+                Blog blog = new Blog(blogId, blogTitle, blogImage, blogDetail, postDate, blogTagId, true, uid);
+                blogS.add(blog);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            // Đóng các tài nguyên
+            if (ps != null) {
+                ps.close();
+            }
+            if (rs != null) {
+                rs.close();
+            }
+        }
+        return blogS;
+    }
+
+    public List<Blog> getBlogFromTag(int tagid) throws SQLException {
+        ArrayList<Blog> blogS = new ArrayList<>();
+        String query = "select * from blog WHERE BlogTagID = ? AND BlogStatus = 1";
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(query);
+            ps.setInt(1, tagid);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                int blogId = rs.getInt("BlogID");
+                String blogTitle = rs.getString("BlogTitle");
+                String blogImage = rs.getString("BlogImage");
+                String blogDetail = rs.getString("BlogDetail");
+                Date postDate = rs.getDate("PostDate");
+                int uid = rs.getInt("UserID");
+                // Tạo đối tượng Blog từ dữ liệu từ cơ sở dữ liệu
+                Blog blog = new Blog(blogId, blogTitle, blogImage, blogDetail, postDate, tagid, true, uid);
+                blogS.add(blog);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Đóng các tài nguyên
+            if (ps != null) {
+                ps.close();
+            }
+            if (rs != null) {
+                rs.close();
+            }
+        }
+        return blogS;
+    }
+
+    public void updateBlog(String title, String image, String detail, String status, String blogtagid, int blogid) {
+        String query = "UPDATE blog set BlogTitle=?, BlogImage=?, PostDate=GETDATE(), BlogDetail=? , BlogStatus=?,BlogTagId=? WHERE BlogId =?";
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(query);
+            ps.setString(1, title);
+            ps.setString(2, image);
+
+            ps.setString(3, detail);
+            ps.setString(4, status);
+            ps.setString(5, blogtagid);
+            ps.setInt(6, blogid);
+            ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Handle the exception appropriately
+        }
+    }
+
+    public void delBlog(int id) {
+        String sql = "delete from blog where BlogId=?";
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, id);
+            ps.execute();
+        } catch (Exception e) {
+        }
+    }
+
+//    public Blog getBlogFromBlogID(int blogID) throws Exception {
+//        Blog blogid = null;
+//        try {
+//            conn = new DBContext().getConnection();
+//            String sql = "SELECT * FROM blog WHERE BlogId = ?";
+//            ps = conn.prepareStatement(sql);
+//            ps.setInt(1, blogID);
+//            rs = ps.executeQuery();
+//            while (rs.next()) {
+//                    blogid = new Blog(rs.getInt(1), rs.getString(2), 
+//                            rs.getString(3), rs.getString(4), 
+//                            rs.getDate(5), rs.getInt(6), 
+//                            rs.getBoolean(7), rs.getInt(8));
+//            }
+//        } catch (SQLException e) {
+//        }
+//        return blogid;
+//    }
+    public Blog getBlogFromBlogID(int blogID) throws Exception {
+        Blog blog = null;
+
+        try {
+            conn = new DBContext().getConnection();
+            String sql = "SELECT * FROM blog WHERE BlogId = ?";
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, blogID);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                blog = new Blog(
+                        rs.getInt("BlogID"),
+                        rs.getString("BlogTitle"),
+                        rs.getString("BlogImage"),
+                        rs.getString("BlogDetail"),
+                        rs.getDate("PostDate"),
+                        rs.getInt("BlogTagID"),
+                        rs.getBoolean("BlogStatus"),
+                        rs.getInt("UserID")
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (ps != null) {
+                ps.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+        return blog;
+    }
+//
+    public static void main(String[] args) throws SQLException, Exception {
+        BlogDAO dao = new BlogDAO();
+        List<Blog> blog = dao.searchBlogs("kiem tra");
+        for (Blog blogTag : blog) {
+            System.out.println(blogTag);
+        }
+//
+//        dao.updateBlog("check", "", "", "1", "1", 34);
+//        System.out.println("success");
+//        dao.getBlogFromBlogID(15);
+//        System.out.println();
+    }
+
+//    public static void main(String[] args) {
+//    BlogDAO dao = new BlogDAO();
+//    try {
+//        Blog blog = dao.getBlogFromBlogID(21);
+//        if (blog != null) {
+//            System.out.println("Blog ID: " + blog.getBlogID());
+//            System.out.println("Title: " + blog.getBlogTitle());
+//            System.out.println("Image: " + blog.getBlogImage());
+//            System.out.println("Detail: " + blog.getBlogDetail());
+//            System.out.println("Date: " + blog.getPostDate());
+//            System.out.println("Tag: " + blog.getBlogTagID());
+//            System.out.println("Status: " + blog.isBlogStatus());
+//            System.out.println("UserID: " + blog.getUserID());
+//        } else {
+//            System.out.println("No blog found with the given ID.");
+//        }
+//    } catch (Exception e) {
+//        e.printStackTrace();
+//    }
+//}
+}
